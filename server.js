295bf65da56f5c12fd8344c06f15deff5eeda9ef
Abhinav Yadav <mr.kumarabhinayadav@gmail.com>
2022-04-17 01:20:11 +0530

    herokupush

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
-app.listen(9000);
+console.log(process.env.PORT);
+app.listen(process.env.PORT);
